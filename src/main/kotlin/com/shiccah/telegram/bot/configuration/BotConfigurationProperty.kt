bc846b6549e0e383df7a6da3643b61716110446d
Dmitry Ilin <shiccah@gmail.com>
2020-12-13 21:16:18 +0300

    remove redundant

--- src/main/kotlin/com/shiccah/telegram/bot/configuration/BotConfigurationProperty.kt
+++ src/main/kotlin/com/shiccah/telegram/bot/configuration/BotConfigurationProperty.kt
@@ -2,7 +2,6 @@ package com.shiccah.telegram.bot.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
-import org.telegram.telegrambots.bots.DefaultBotOptions
 
 @Component
 @ConfigurationProperties(prefix = "telegrambot")
@@ -10,7 +9,4 @@ class BotConfigurationProperty {
     var webHookPath: String? = null
     var userName: String? = null
     var botToken: String? = null
-    var proxyType: DefaultBotOptions.ProxyType? = null
-    var proxyHost: String? = null
-    var proxyPort = 0
 }
